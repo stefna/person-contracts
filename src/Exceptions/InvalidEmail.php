e91b22c21f32ff28e8104b5d2654a7b75a5ca53c
Andreas Sundqvist <andreas@stefna.is>
2026-01-14 14:20:19 +0100

    Fix deprecations

--- src/Exceptions/InvalidEmail.php
+++ src/Exceptions/InvalidEmail.php
@@ -6,7 +6,7 @@ use Throwable;
 
 class InvalidEmail extends \DomainException
 {
-	public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+	public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
 	{
 		parent::__construct($message ?: 'Invalid email', $code, $previous);
 	}
--- src/Exceptions/InvalidSsn.php
+++ src/Exceptions/InvalidSsn.php
@@ -6,7 +6,7 @@ use Throwable;
 
 class InvalidSsn extends \DomainException
 {
-	public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+	public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
 	{
 		parent::__construct($message ?: 'Invalid ssn', $code, $previous);
 	}
