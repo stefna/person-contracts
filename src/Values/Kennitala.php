2e4cb52e482550c0bf21cee6a6233fa59fa6b7fc
Andreas Sundqvist <andreas@stefna.is>
2026-01-14 14:20:10 +0100

    Remove checksum check for Kt
    From 18 feb 2026 it will no longer be used and new kennitals will
    not "include" it anymore
    
    https://www.skra.is/folk/eg-i-thjodskra/um-kennitolur/

--- src/Values/Kennitala.php
+++ src/Values/Kennitala.php
@@ -6,29 +6,9 @@ use Stefna\PersonContract\Exceptions\InvalidSsn;
 
 final readonly class Kennitala implements Ssn, \JsonSerializable
 {
-	private const KENNITALA_INVALID = 'Invalid kennitala';
 	private const KENNITALA_NOT_NUMERIC = 'Invalid kennitala. Provided ssn is not numeric';
 	private const KENNITALA_TOO_SHORT = 'Invalid kennitala. Provided ssn is to short. Expected 10 chars';
 	private const KENNITALA_TOO_LONG = 'Invalid kennitala. Provided ssn is to long. Expected 10 chars';
-
-	private static function validateChecksum(string $value): bool
-	{
-		$sum = (int)$value[0] * 3;
-		$sum += (int)$value[1] * 2;
-		$sum += (int)$value[2] * 7;
-		$sum += (int)$value[3] * 6;
-		$sum += (int)$value[4] * 5;
-		$sum += (int)$value[5] * 4;
-		$sum += (int)$value[6] * 3;
-		$sum += (int)$value[7] * 2;
-
-		$verificationNumber = 11 - ($sum % 11);
-		if ($verificationNumber === 11) {
-			$verificationNumber = 0;
-		}
-
-		return $verificationNumber === (int)$value[8];
-	}
 
 	public static function isValid(string $ssn): bool
 	{
@@ -44,10 +24,6 @@ final readonly class Kennitala implements Ssn, \JsonSerializable
 
 		if (\strlen($ssn) > 10) {
 			throw new InvalidSsn(self::KENNITALA_TOO_LONG);
-		}
-
-		if (!self::validateChecksum($ssn)) {
-			throw new InvalidSsn(self::KENNITALA_INVALID);
 		}
 
 		return true;
